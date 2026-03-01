ca613484bb5c402df467307018de88ea50604eb0
Husayn <husaynmuhammadyahyo1606@gmail.com>
2026-03-01 13:25:41 +0500

    ss

--- core/urls.py
+++ core/urls.py
@@ -24,6 +24,7 @@ urlpatterns = [
     path('blogs/', include('blogs.urls', namespace='blogs')),
     path('products/', include('products.urls', namespace='products')),
     path('', include('shared.urls', namespace='shared')),
+    path('users/', include('users.urls', namespace='users')),
     path('', include('about.urls'))
 ]
 
